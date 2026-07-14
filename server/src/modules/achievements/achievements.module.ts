08a1d4a840ea0d5779d57f1c2607bcd655fc2278
Reivhell <davidgame1945@gmail.com>
2026-07-14 22:10:25 +0800

    feat(achievements): add NestJS module, service, controller, event gateway

--- server/src/modules/achievements/achievements.module.ts
+++ server/src/modules/achievements/achievements.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit, Inject } from '@nestjs/common';
 import { AchievementsController } from './achievements.controller';
 import { AchievementsService } from './achievements.service';
 import { AchievementsEventGateway } from './achievements.gateway';
+import { DRIZZLE } from '../../common/database/drizzle.provider';
+import type { DbInstance } from '../../drizzle';
+import { seedAchievements } from './seed';
 
 @Module({
   controllers: [AchievementsController],
   providers: [AchievementsService, AchievementsEventGateway],
   exports: [AchievementsEventGateway],
 })
-export class AchievementsModule {}
+export class AchievementsModule implements OnModuleInit {
+  constructor(@Inject(DRIZZLE) private db: DbInstance) {}
+
+  async onModuleInit() {
+    await seedAchievements(this.db);
+  }
+}
